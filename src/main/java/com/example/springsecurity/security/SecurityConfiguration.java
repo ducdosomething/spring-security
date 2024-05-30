8567e41d47f62d9bcfde851fd68db04e5b49a2b1
ducdosomething <duclehoang211201@gmail.com>
2024-05-30 09:15:51 +0700

    spring security

---
+++ src/main/java/com/example/springsecurity/security/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.example.springsecurity.security;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    // Xác nhận thông tin đăng nhập của người dùng
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}12345").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}12345").roles("ADMIN");
+    }
+
+    // Cấu hin ba mật dựa trên yêu cầu của HTTP
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/user**").hasRole("USER")
+                .requestMatchers("/admin**").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+}
